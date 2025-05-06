606579d0ff3ad94b24eeb368bbcb68d048ae26d4
Nayra92 <naira.hashem@gmail.com>
2025-05-06 19:54:57 +0300

    modified:   lib/constants.dart 	modified:   lib/show_data.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
-Color mainColor = Color(0xff1C6974);
+Color mainColor = Color.fromARGB(255, 230, 236, 114);
 Color secondColor = Color(0xff1E2852);
 Color greyColor = Color(0xffD9D9D9);
 Color buttonsColor = Color(0xff14484E);
--- lib/show_data.dart
+++ lib/show_data.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:students_projects/constants.dart';
-
+import 'package:students_projects/register_new_product.dart';
 
 class ShowData extends StatefulWidget {
   const ShowData({super.key});
@@ -9,64 +9,192 @@ class ShowData extends StatefulWidget {
   @override
   State<ShowData> createState() => _ShowDataState();
 }
-
+String _nameInitial = "";
 class _ShowDataState extends State<ShowData> {
+  TextEditingController _name = TextEditingController(
+    // text: _nameInitial
+  );
+  TextEditingController _subtitle = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-                .collection("books")
-                .snapshots(), 
-        builder: (BuildContext, AsyncSnapshot<QuerySnapshot> snapshots){
-     
-          if(snapshots.connectionState == ConnectionState.waiting){
-            return CircularProgressIndicator();
-          }
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return AlertDialog(
+                                            title: Text("Delete User"),
+                                            content: SizedBox(
+                                              height: 150,
+                                              child: RegisterNewProduct()),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text("Cancel")),
+                                              TextButton(
+                                                  onPressed: ()  {
+                                                    
+                                                  },
+                                                  child: Text("Ok"))
+                                            ],
+                                          );});
+      },
+      child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: RegisterNewProduct(),
+          ),
+          Expanded(
+            flex: 2,
+            child: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection("books").snapshots(),
+                builder:
+                    (BuildContext, AsyncSnapshot<QuerySnapshot> snapshots) {
+                  if (snapshots.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
 
-        final booksData = snapshots.data!.docs;
-        
-           if (booksData.isEmpty){
-             return const Text("No Data Found!");
-          }
-          else {
-          return ListView.builder(
-            itemCount: booksData.length,
-            itemBuilder: (context, index){
-              return Column(
-                children: [
-                  ListTile(
-                    tileColor: mainColor,
-                    title:  Text(
-                        booksData[index]["author"],
-                        style: TextStyle(
-                          fontSize: 34
-                        ),),
-                  
-                  subtitle:Text(booksData[index]["releaseDate"]),
-                  ),
-                  SizedBox(height: 20,)
-                ],
-              );
-              
-              // Column(
-              //   children: [
-              //     Text(
-              //       booksData[index]["author"],
-              //       style: TextStyle(
-              //         fontSize: 34
-              //       ),),
-              //     Text(booksData[index]["releaseDate"]),
-              //   ],
-              // );
-            });
-          
-          // Text(booksData[1]["author"]);
-          }
+                  final booksData = snapshots.data!.docs;
 
-        }
-        
-        ),
+                  if (booksData.isEmpty) {
+                    return const Text("No Data Found!");
+                  } else {
+                    return ListView.builder(
+                        itemCount: booksData.length,
+                        itemBuilder: (context, index) {
+                          _nameInitial = booksData[index]["author"];
+                          return Column(
+                            children: [
+                              ListTile(
+                                tileColor: mainColor,
+                                title: Text(
+                                  booksData[index]["author"],
+                                  style: TextStyle(fontSize: 34),
+                                ),
+                                subtitle: Text(booksData[index]["releaseDate"]),
+                                leading: IconButton(
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return AlertDialog(
+                                            title: Text("Edit User"),
+                                            content: Column(
+                                              children: [
+                                                TextFormField(
+                                                  controller: _name,
+                                                  decoration: InputDecoration(
+                                                    hintText:  booksData[index]["author"]
+                                                  ),
+                                                  
+                                                ),
+                                                TextFormField(
+                                                  controller: _subtitle,
+                                            
+                                                ),
+                                              ],
+                                            ),
+
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text("Cancel")),
+
+                                              TextButton(
+                                                  onPressed: () async {
+
+                                                    if(_name.text.isEmpty || _subtitle.text.isEmpty){
+                                                      print("error");
+                                                    }
+                                                    else {
+                                                    await FirebaseFirestore.instance
+                                                    .collection("books")
+                                                    .doc(booksData[index].id)
+                                                    .update({
+                                                      "author": _name.text,
+                                                      "releaseDate": _subtitle.text
+                                                    });
+                                                    
+                                                    Navigator.pop(context);
+                                                    }
+                                                    
+                                                  
+
+                                                  },
+                                                  child: Text("Update")),
+                                            ],
+                                           
+                                          );
+                                        });
+                                  },
+                                  icon: Icon(Icons.edit),
+                                ),
+                                trailing: IconButton(
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return AlertDialog(
+                                            title: Text("Delete User"),
+                                            content: Text(
+                                                "This User will be removed forever"),
+                                            actions: [
+                                              TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text("Cancel")),
+                                              TextButton(
+                                                  onPressed: () async {
+                                                    await FirebaseFirestore
+                                                        .instance
+                                                        .collection("books")
+                                                        .doc(
+                                                            booksData[index].id)
+                                                        .delete();
+                                                        Navigator.pop(context);
+                                                  },
+                                                  child: Text("Delete"))
+                                            ],
+                                          );
+                                        });
+                                  },
+                                  icon: Icon(Icons.delete),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              )
+                            ],
+                          );
+
+                          // Column(
+                          //   children: [
+                          //     Text(
+                          //       booksData[index]["author"],
+                          //       style: TextStyle(
+                          //         fontSize: 34
+                          //       ),),
+                          //     Text(booksData[index]["releaseDate"]),
+                          //   ],
+                          // );
+                        });
+
+                    // Text(booksData[1]["author"]);
+                  }
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
